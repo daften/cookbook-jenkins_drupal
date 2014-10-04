364f75a9fa1519434091dc20649a7f3750f35704
daften <dieterblomme@gmail.com>
2014-10-04 20:46:31 +0200

    Update version number

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'info@picabit.be'
 license          'Apache 2.0'
 description      'Installs/Configures jenkins with drupal specific plugins'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.2'
+version          '0.1.3'
 recipe           "jenkins_drupal", "Installs jenkins with all extras for drupal testing"
 recipe           "jenkins_drupal::jenkins", "Installs jenkins as a package with some basic plugins"
 recipe           "jenkins_drupal::coding_standards", "Install jenkins plugins and packages for code style testing"
